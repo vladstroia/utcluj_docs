3f155fd9f3b6d6607574406c2285b33e2d87d79e
vladstroia <vlad.stroia@gmail.com>
2022-10-21 21:34:51 +0200

    a

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from docxtpl import DocxTemplate
 import pandas as pd
 df = pd.read_csv('template_plan_invatamant.csv')
 doc = DocxTemplate("template-fisa-disciplinei.docx")
-context = {}
-for i in df:
-    context[i] = df[i]
-
-
-doc.render(context)
-doc.save("generated_doc.docx")
+for i in range(20):
+    context = {}
+    for item in df.loc[[i]]:
+        context[item] =df.loc[[i]][item].values[0]
+        if context[item] == 'nan':
+            context[item] = ''
+    doc.render(context)
+    doc.save(f"./files/generated_doc{context['p1_8']}.docx")
